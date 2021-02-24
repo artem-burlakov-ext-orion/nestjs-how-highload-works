bc7076b73099354c56a172beca72fdcfac4fee06
Artem <eifory@ideadeploy.com>
2021-02-24 21:55:09 +0300

    fix config 2

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -1,11 +1,6 @@
 import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const typeOrmConfig: TypeOrmModuleOptions = {
-  type: 'postgres',
-  host: 'localhost',
-  port: 5432,
-  username: 'postgres',
-  password: '123456',
-  database: 'hhw',
+  
   autoLoadEntities: true,
 }
