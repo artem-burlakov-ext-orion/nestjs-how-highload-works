8e20eab532dd20992fa60236efbc3593989958d6
Artem <eifory@ideadeploy.com>
2021-02-27 01:36:32 +0300

    add stats repository

--- src/stats/stats.controller.ts
+++ src/stats/stats.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post } from '@nestjs/common';
 
 
 @Controller('stats')
 export class StatsController {
+  constructor(private statsService: StatsService) {}
+
+  @Post()
+  createStats(@Body createStatsDto: CreateStatsDto) {
+    return this.statsService()
+  }
   
 }
--- src/stats/stats.service.ts
+++ src/stats/stats.service.ts
@@ -1,4 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { StatsRepository } from './stats.repository';
 
 @Injectable()
-export class StatsService {}
+export class StatsService {
+  constructor(
+  @InjectRepository(StatsRepository)
+  private statsRepository: StatsRepository) {}
+
+  async createStats() {
+    
+  }
+
+}
